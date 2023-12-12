10420800a8fb9f90b00235561f9711e36e7e3204
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2023-12-12 10:06:06 -0300

    databinding course

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrl: './cockpit.component.css'
+})
+export class CockpitComponent {
+ @Output() serverCreated = new EventEmitter<{serverName: string,  serverContent: string}>();
+ @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // newServerName = '';
+  // newServerContent = '';
+@ViewChild('serverContentInput') serverContentInput;
+
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    })
+    // this.serverCreated.emit({
+    //   serverName: nameInput.value, 
+    //   serverContent: this.newServerContent});
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    })
+    // this.blueprintCreated.emit({
+    //   serverName: nameInput.value, 
+    //   serverContent: this.newServerContent});
+    // ;
+  }
+}
